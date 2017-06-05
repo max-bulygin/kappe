3fc048d2ba812a3fad273293df1653239c6c9fa0
Max Bulygin <maksym.bulygin@gmail.com>
2017-06-05 14:36:51 +0300

    added no scale for mobile

--- wp_theme/header.php
+++ wp_theme/header.php
@@ -1,10 +1,11 @@
 <!doctype html>
 <html lang="<?php language_attributes(); ?>">
 <head>
+     <?php $no_scale = ', maximum-scale=1.0, user-scalable=no'; ?>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="<?php bloginfo('description'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1<?php echo (wp_is_mobile() ? $no_scale : ''); ?>">
     <title><?php bloginfo('name'); ?><?php wp_title('|'); ?></title>
 
     <!-- Disable tap highlight on IE -->
