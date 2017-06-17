d10a72e3db17a7f334b2ff28bfa494e08c2ad1cf
Max Bulygin <maksym.bulygin@gmail.com>
2017-06-18 01:36:50 +0300

    markup tweaks

--- wp_theme/contacts.php
+++ wp_theme/contacts.php
@@ -20,9 +20,7 @@ get_header(); ?>
 
   <div class="contacts">
     <h3 class="heading"><?php _e( 'Get in touch', 'kappe' ); ?></h3>
-    <p class="paragraph">
-      <?php echo $post->post_content; ?>
-    </p>
+    <?php echo $post->post_content; ?>
     <h3 class="heading"><?php _e( 'Send a message', 'kappe' ); ?></h3>
 
     <?php echo do_shortcode( $shortcode ); ?>
